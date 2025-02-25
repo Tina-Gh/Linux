d681207d219fdea45bfdfac57379536d42af8949
Tina-Gh <secretdaise3@gmail.com>
2025-02-25 16:42:15 -0500

    Updating files

---
+++ SebastianRaschka_Book/chapter1/hello.c
@@ -0,0 +1,15 @@
+/*
+To run:
+>> gcc -o hello hello.c 
+>> ./hello 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello World\n");
+    exit(0);
+
+}
---
+++ SebastianRaschka_Book/chapter1/lib.h
@@ -0,0 +1,6 @@
+/*
+THis is lib.h. It declares teh functins "fred" and "George" for users.
+*/
+
+void fred(int);
+void george(char*);
